d18da9366caf1d10e275430d94afad7986778b27
Akash <120485931+akashgawand@users.noreply.github.com>
2025-10-17 03:07:50 +0530

    Update AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,6 +1,5 @@
 import { createContext } from 'react';
 
-// Only export the context object from this file (no components) so fast refresh works.
 export const AuthContext = createContext();
 
 export default AuthContext;
