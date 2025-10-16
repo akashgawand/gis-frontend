9663dca9d87f5c67609514a928553ea09c500d73
Akash <120485931+akashgawand@users.noreply.github.com>
2025-10-17 01:38:20 +0530

    Update API_URL to point to the correct endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://github.com/akashgawand/gis-backend/blob/main/server.js";
+const API_URL = "https://github.com/akashgawand/gis-backend/blob/main/server.js/api";
 
 const api = axios.create({
   baseURL: API_URL,
